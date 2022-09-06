fe4cb816e810507554eb244e025bc4e558b19a3b
Oliver Forral <intrepion@gmail.com>
2022-09-06 15:32:50 -0600

    green

---
+++ src/item.rs
@@ -0,0 +1,7 @@
+pub struct Item;
+
+impl Default for Item {
+    fn default() -> Self {
+        Self {}
+    }
+}
--- tests/item.rs
+++ tests/item.rs
@@ -1,6 +1,8 @@
+use intrepion_to_do_list_library_rust::item::Item;
+
 #[test]
 fn item_should_have_title_when_created() {
     let _expected = "Untitled";
 
-    let item = Item::default();
+    let _item = Item::default();
 }
