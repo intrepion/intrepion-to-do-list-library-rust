c7ecc0b59b22c6a935b3a9877e9a127694860903
Oliver Forral <intrepion@gmail.com>
2022-09-06 15:38:15 -0600

    refactor

--- tests/item.rs
+++ tests/item.rs
@@ -12,8 +12,10 @@ fn item_should_have_title_when_created() {
 
 #[test]
 fn item_should_have_is_done_false_when_created() {
-    let _expected = false;
+    let expected = false;
 
     let item = Item::default();
-    let _actual = item.is_done;
+    let actual = item.is_done;
+
+    assert_eq!(expected, actual);
 }
