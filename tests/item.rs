624695911516507ae089650e00f4832462da1116
Oliver Forral <intrepion@gmail.com>
2022-09-06 16:35:12 -0600

    refactor

--- tests/item.rs
+++ tests/item.rs
@@ -1,31 +1,34 @@
-use intrepion_to_do_list_library_rust::item::Item;
+#[cfg(test)]
+mod test_item_should_have {
+    use intrepion_to_do_list_library_rust::item::Item;
 
-#[test]
-fn item_should_have_title_when_created() {
-    let expected = "Untitled";
+    #[test]
+    fn title_when_created() {
+        let expected = "Untitled";
 
-    let item = Item::default();
-    let actual = item.title;
+        let item = Item::default();
+        let actual = item.title;
 
-    assert_eq!(expected, actual);
-}
+        assert_eq!(expected, actual);
+    }
 
-#[test]
-fn item_should_have_is_done_false_when_created() {
-    let expected = false;
+    #[test]
+    fn is_done_false_when_created() {
+        let expected = false;
 
-    let item = Item::default();
-    let actual = item.is_done;
+        let item = Item::default();
+        let actual = item.is_done;
 
-    assert_eq!(expected, actual);
-}
+        assert_eq!(expected, actual);
+    }
 
-#[test]
-fn item_should_have_is_visible_true_when_created() {
-    let expected = true;
+    #[test]
+    fn is_visible_true_when_created() {
+        let expected = true;
 
-    let item = Item::default();
-    let actual = item.is_visible;
+        let item = Item::default();
+        let actual = item.is_visible;
 
-    assert_eq!(expected, actual);
+        assert_eq!(expected, actual);
+    }
 }
