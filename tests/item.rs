175c4587483196af3c784339d4c3c64c569c7135
Oliver Forral <intrepion@gmail.com>
2022-09-06 15:34:08 -0600

    red

--- tests/item.rs
+++ tests/item.rs
@@ -4,5 +4,6 @@ use intrepion_to_do_list_library_rust::item::Item;
 fn item_should_have_title_when_created() {
     let _expected = "Untitled";
 
-    let _item = Item::default();
+    let item = Item::default();
+    let actual = item.title;
 }
