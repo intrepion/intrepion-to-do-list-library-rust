f0c07db25cb429203c8d81b5d823730359f8f81f
Oliver Forral <intrepion@gmail.com>
2022-09-06 15:40:25 -0600

    red

--- tests/item.rs
+++ tests/item.rs
@@ -22,8 +22,10 @@ fn item_should_have_is_done_false_when_created() {
 
 #[test]
 fn item_should_have_is_visible_true_when_created() {
-    let _expected = true;
+    let expected = true;
 
     let item = Item::default();
-    let _actual = item.is_visible;
+    let actual = item.is_visible;
+
+    assert_eq!(expected, actual);
 }
